8ff11e3f8fdce750e36ced3d97da233db2e4b0c8
giovrizzo99 <119448273+giovrizzo99@users.noreply.github.com>
2025-03-09 18:16:05 +0100

    Update ProgressBarRound.js

--- ProgressBarRound.js
+++ ProgressBarRound.js
@@ -29,8 +29,8 @@
             position: absolute;
             top: 50%;
             transform: translate(-50%, -50%);
-            width: 35px;
-            height: 35px;
+            width: 10%;
+            height: 100%;
             border-radius: 50%;
             background-color: #05446b; /* Dark blue inside */
             border: 3px solid #69a8e2; /* Light blue border */
