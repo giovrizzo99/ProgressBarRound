52f9b69f8c12d96f1ccb51de5017d36d788e085e
giovrizzo99 <119448273+giovrizzo99@users.noreply.github.com>
2025-03-17 12:58:55 +0100

    Update ProgressBarRound_Builder.js

--- ProgressBarRound_Builder.js
+++ ProgressBarRound_Builder.js
@@ -1,123 +1,113 @@
 (function () {
     let template = document.createElement("template");
     template.innerHTML = `
-    <br>
     <style>
-        #form {
-            font-family: Arial, sans-serif;
-            width: 400px;
-            margin: 0 auto;
-        }
-
-        a {
-            text-decoration: none;
-        }
-
-        table {
+        :host {
+            display: block;
             width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 10px;
+            height: 100%;
+            position: relative;
         }
 
-        td {
-            padding: 1px;
-            text-align: left;
-            font-size: 13px;
-        }
-
-        input {
+        .progress-container {
             width: 100%;
-            padding: 10px;
-            border: 2px solid #ccc;
+            height: 100%;
+            background-color: var(--empty-bar-color, #f0f0f0);
             border-radius: 5px;
-            font-size: 13px;
-            box-sizing: border-box;
-            margin-bottom: 10px;
+            overflow: hidden;
+            position: relative;
         }
 
-        input[type="color"] {
-            -webkit-appearance: none;
-            border: none;
-            width: 32px;
-            height: 32px;
-        }
-        input[type="color"]::-webkit-color-swatch-wrapper {
-            padding: 0;
-        }
-        input[type="color"]::-webkit-color-swatch {
-            border: none;
+        .progress-bar {
+            height: 100%;
+            width: 0%;
+            background: linear-gradient(90deg, var(--start-color, #05446b), var(--end-color, #69a8e2));
+            transition: width 0.5s ease-in-out;
         }
 
-        input[type="submit"] {
-            background-color: #487cac;
+        .progress-indicator {
+            position: absolute;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: auto;
+            height: 90%;
+            aspect-ratio: 1 / 1;
+            border-radius: 50%;
+            background-color: var(--bar-color, #05446b);
+            border: 3px solid var(--end-color, #69a8e2);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 10px;
+            font-weight: bold;
             color: white;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            font-size: 14px;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        #label {
-            width: 140px;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
         }
     </style>
-    
-    <form id="form">
-        <table>
-            <tr>
-                <td>
-                    <p>Percentage</p>
-                    <input id="builder_percentage" type="number" min="0" max="100" placeholder="Enter Percentage">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Empty Bar Color</p>
-                    <input id="builder_emptyBarColor" type="color" placeholder="Select Empty Bar Color">
-                </td>
-            </tr>
-        </table>
-        <input value="Update Settings" type="submit">
-        <br>
-    </form>
+
+    <div class="progress-container">
+        <div class="progress-bar"></div>
+        <div class="progress-indicator">0%</div>
+    </div>
     `;
 
-    class StraightProgressBarBuilderPanel extends HTMLElement {
+    class StraightProgressBar extends HTMLElement {
         constructor() {
             super();
-            this._shadowRoot = this.attachShadow({ mode: "open" });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-            this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+            let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(template.content.cloneNode(true));
+            this._props = {};
         }
 
-        _submit(e) {
-            e.preventDefault();
-            this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                detail: {
-                    properties: {
-                        percentage: this.percentage,
-                        emptyBarColor: this.emptyBarColor
-                    },
-                },
-            }));
+        async connectedCallback() {
+            this.updateProgress();
         }
 
-        set percentage(value) {
-            this._shadowRoot.getElementById("builder_percentage").value = value;
-        }
-        get percentage() {
-            return this._shadowRoot.getElementById("builder_percentage").value;
+        async updateProgress() {
+            const progressBar = this.shadowRoot.querySelector(".progress-bar");
+            const progressIndicator = this.shadowRoot.querySelector(".progress-indicator");
+            const progressContainer = this.shadowRoot.querySelector(".progress-container");
+
+            const percentage = this._props.percentage || 0;
+            const barColor = this._props.barColor || "#05446b";
+            const startColor = this._props.startColor || "#05446b";
+            const endColor = this._props.endColor || "#69a8e2";
+            const emptyBarColor = this._props.emptyBarColor || "#f0f0f0";
+
+            var h = progressContainer.getBoundingClientRect().height;
+            var w = progressContainer.getBoundingClientRect().width;
+            var half_ratio_h_w = (h > 6) * 50 * (h - 6) / w;
+            var ratio_border_height = 100 * (h >= 6) * 6 / h;
+            var half_ratio_border_width = 100 * (w >= 6) * 3 / w;
+            var new_height = Math.floor(100 - ratio_border_height);
+            progressIndicator.style.height = `${new_height}%`;
+
+            var adjperc = percentage;
+            if (percentage < half_ratio_border_width + half_ratio_h_w) {
+                adjperc = Math.ceil(half_ratio_border_width + half_ratio_h_w);
+            }
+            if (percentage > 100 - half_ratio_border_width - half_ratio_h_w) {
+                adjperc = Math.floor(100 - half_ratio_border_width - half_ratio_h_w);
+            }
+            console.log(adjperc, half_ratio_border_width, half_ratio_h_w, percentage, h, progressIndicator.style.height);
+
+            progressBar.style.width = `${percentage}%`;
+            progressBar.style.background = `linear-gradient(90deg, ${startColor}, ${endColor})`;
+            this.shadowRoot.querySelector(".progress-container").style.backgroundColor = emptyBarColor;
+            progressIndicator.innerText = `${percentage}%`;
+            progressIndicator.style.left = `${adjperc}%`;
+            progressIndicator.style.backgroundColor = startColor;
+            progressIndicator.style.borderColor = endColor;
         }
 
-        set emptyBarColor(value) {
-            this._shadowRoot.getElementById("builder_emptyBarColor").value = value;
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = { ...this._props, ...changedProperties };
         }
-        get emptyBarColor() {
-            return this._shadowRoot.getElementById("builder_emptyBarColor").value;
+
+        onCustomWidgetAfterUpdate(changedProperties) {
+            this.updateProgress();
         }
     }
 
-    customElements.define("com-gr-progressbarround-builder", StraightProgressBarBuilderPanel);
+    customElements.define("com-gr-progressbarround", StraightProgressBar);
 })();
